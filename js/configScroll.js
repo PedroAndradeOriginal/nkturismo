a3768e81c04826a3ca960d73bd2b513ddfa76741
Pedro Andrade <pedrofreitasdeandrade@gmail.com>
2022-11-26 20:48:41 -0300

    Otimização nos cookies

--- js/configScroll.js
+++ js/configScroll.js
@@ -1,23 +1,23 @@
-window.sr = ScrollReveal({ reset: true })
+window.sr = ScrollReveal({ reset: true });
 
 ScrollReveal({
-    distance: '150px',
+    distance: '50px',
     duration: 800,
     origin: 'top',
     reset: false
-})
+});
 
 // Delay
 
-sr.reveal('.carousel', { delay: 300 })
-sr.reveal('.middleImg', { delay: 500 })
-sr.reveal('.sec3', { delay: 700 })
-sr.reveal('.bottomBanner', { delay: 500 })
-sr.reveal('#section01', { delay: 300 })
-sr.reveal('#img-redes', { delay: 500 })
-sr.reveal('#article02', { delay: 700 })
-sr.reveal('.section-1-QS', { delay: 300 })
-sr.reveal('.section-2-QS', { delay: 700 })
-sr.reveal('.section-3-QS', { delay: 500 })
-sr.reveal('.section-4-QS', { delay: 700 })
-sr.reveal('.titulo', { delay: 100 })
+sr.reveal('.carousel', { delay: 100 });
+sr.reveal('.middleImg', { delay: 500 });
+sr.reveal('.sec3', { delay: 700 });
+sr.reveal('.bottomBanner', { delay: 500 });
+sr.reveal('#section01', { delay: 100 });
+sr.reveal('#img-redes', { delay: 500 });
+sr.reveal('#article02', { delay: 700 });
+sr.reveal('.section-1-QS', { delay: 100 });
+sr.reveal('.section-2-QS', { delay: 700 });
+sr.reveal('.section-3-QS', { delay: 500 });
+sr.reveal('.section-4-QS', { delay: 700 });
+sr.reveal('.titulo', { delay: 100 });
