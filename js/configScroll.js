c3a2f638d1db0a1d340a2c82b259c84e4cf5c5cb
Pedro Andrade <pedrofreitasdeandrade@gmail.com>
2022-11-27 15:26:43 -0300

    Finalização do css e js

--- js/configScroll.js
+++ js/configScroll.js
@@ -10,14 +10,20 @@ ScrollReveal({
 // Delay
 
 sr.reveal('.carousel', { delay: 100 });
-sr.reveal('.middleImg', { delay: 500 });
-sr.reveal('.sec3', { delay: 700 });
-sr.reveal('.bottomBanner', { delay: 500 });
+sr.reveal('.middleImg', { delay: 300 });
+sr.reveal('.sec3', { delay: 300 });
+sr.reveal('.bottomBanner', { delay: 300 });
 sr.reveal('#section01', { delay: 100 });
-sr.reveal('#img-redes', { delay: 500 });
-sr.reveal('#article02', { delay: 700 });
+sr.reveal('#img-redes', { delay: 300 });
+sr.reveal('#article02', { delay: 300 });
 sr.reveal('.section-1-QS', { delay: 100 });
-sr.reveal('.section-2-QS', { delay: 700 });
-sr.reveal('.section-3-QS', { delay: 500 });
-sr.reveal('.section-4-QS', { delay: 700 });
+sr.reveal('.section-2-QS', { delay: 300 });
+sr.reveal('.section-3-QS', { delay: 300 });
+sr.reveal('.section-4-QS', { delay: 300 });
 sr.reveal('.titulo', { delay: 100 });
+sr.reveal('.sec1', { delay: 100 });
+sr.reveal('.sec2', { delay: 300 });
+sr.reveal('.sec3', { delay: 300 });
+sr.reveal('.sec4', { delay: 300 });
+sr.reveal('.master-sec5', { delay: 300 });
+
